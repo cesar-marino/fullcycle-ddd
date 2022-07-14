9c7db0a6540e5b66971cf6e5367efb5fee09fde9
César Marino <cesarmarino00@gmail.com>
2022-07-14 20:22:02 -0300

    add configurations to tests

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const address = new Address("Rau Faria Lemos", 1137, "87130-000", "Ivatuba");
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item1", 10);
-const item2 = new OrderItem("2", "Item2", 15);
+const item1 = new OrderItem("1", "Item1", 10, "p1", 2);
+const item2 = new OrderItem("2", "Item2", 15, "p2", 3);
 const order = new Order("1", "123", [item1, item2]);
