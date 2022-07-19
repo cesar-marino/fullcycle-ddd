1271af72231e1c7a1ae3a98d0441ad1a0bcb356b
César Marino <cesarmarino00@gmail.com>
2022-07-19 14:54:25 -0300

    changed correct imports

--- src/domain/customer/event/customer-events.spec.ts
+++ src/domain/customer/event/customer-events.spec.ts
@@ -1,4 +1,4 @@
-import EventDispatcher from "../../@shared/event-dispatcher";
+import EventDispatcher from "../../@shared/event/event-dispatcher";
 import CustomerAddressChangedEvent from "./customer-address-changed.event";
 import CustomerCreatedEvent from "./customer-created.event";
 import SendConsoleLogChangeAddress from "./handler/send-console-log-change-address.handler";
--- src/domain/product/event/handler/send-email-when-product-is-created.handler.ts
+++ src/domain/product/event/handler/send-email-when-product-is-created.handler.ts
@@ -1,8 +1,7 @@
-import EventHandlerInterface from "../../../@shared/event-handler.interface";
+import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
 import ProductCreatedEvent from "../product-created.event";
 
-export default class SendEmailWhenProductIsCreatedHandler
-    implements EventHandlerInterface<ProductCreatedEvent> {
+export default class SendEmailWhenProductIsCreatedHandler implements EventHandlerInterface<ProductCreatedEvent> {
     handle(event: ProductCreatedEvent): void {
         console.log("Send email to ...");
     }
